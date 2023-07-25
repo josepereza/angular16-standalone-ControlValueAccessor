c571b5a523b499c25eec7d54ddde4fd15fa4653c
josepereza <josepereza66@gmail.com>
2023-07-25 09:50:09 +0200

    first commit

--- src/app/components/custom/custom.component.ts
+++ src/app/components/custom/custom.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailComponent } from '../email/email.component';
-import { ReactiveFormsModule, FormBuilder, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormsModule, Validators } from '@angular/forms';
+import { StarRatingComponent } from '../star-rating/star-rating.component';
 
 @Component({
   selector: 'app-custom',
   standalone: true,
-  imports: [CommonModule, EmailComponent,ReactiveFormsModule,FormsModule],
+  imports: [CommonModule, EmailComponent,ReactiveFormsModule,FormsModule, StarRatingComponent],
   templateUrl: './custom.component.html',
   styleUrls: ['./custom.component.css']
 })
@@ -16,12 +17,14 @@ export class CustomComponent {
   }
   clienteForm = this.fb.nonNullable.group({
     firstName: [''],
-    email: ['']  
+    email: ['']  ,
+    rating: [4 , Validators.min(3)]
   });
-
+  
   enviar(){
     console.log('enviar')
     console.log(this.clienteForm.value)
+    console.log('valid--> ', this.clienteForm.valid);
   }
 
   
